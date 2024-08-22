79b554092f9437d9bc92823c5b6f5c5a5f85d315
bsshin71 <beumsu@gmail.com>
2024-08-22 09:14:09 +0000

    fix redo usage error

--- sql/8_redolog_dir.sql
+++ sql/8_redolog_dir.sql
@@ -1,4 +1,4 @@
  select case @@innodb_log_group_home_dir
-        when './' then  concat(@@datadir,'#innodb_redo')
-        else concat(@@innodb_log_group_home_dir,'#innodb_redo')
+        when './' then  concat(@@datadir,'/','#innodb_redo')
+        else concat(@@innodb_log_group_home_dir,'/','#innodb_redo')
         end  as redo_dir
